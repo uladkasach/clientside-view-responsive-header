19b6648dca6d1481bd4aaa835cb998abc52d8e4e
UladKasach <nappa300@gmail.com>
2018-05-16 13:30:38 -0400

    update demo to be functional with large example

--- demo/src/init/header/index.js
+++ demo/src/init/header/index.js
@@ -1,6 +1,6 @@
 var view_loader = require("clientside-view-loader");
 var header_structure = require("./header_structure.json");
 module.exports = (async function(){
-    var dom = await view_loader.load("/_views/global/header_builder").generate(header_structure);
+    var dom = await view_loader.load("clientside-view-responsive-header").generate(header_structure);
     return dom;
 })()
